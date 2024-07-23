a7255f84e3def76479d65bfa3f843bb932dd6844
Sekhar Pulakanti <114207278+sekharsamba@users.noreply.github.com>
2024-07-23 10:26:10 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,17 @@
+# Introduction to Python
+'''
+
+1.Programmimg Language
+2.Python is an interpreted, object-oriented, high-level programming language with dynamic semantics.
+3.Developed By GVR(Gudio Van Rossum) in 1991 was a Dutch Progremmer
+4.Python uses end with .py extension
+5. Python is user friendly Language
+6. Python this name is taken from MONTY PYTHON FLYING CIRCUS Which a BBC show
+7.Python has Large Community
+8.Python has Large or Standard Labries ,Pacages,Modules and FrameWorks
+
+
+'''
+# My First Program In Python
+
+print("Welcome to python")
