4ce923cc8644b7b3f0a7cecdd2982378f9298c7f
Sekhar Pulakanti <114207278+sekharsamba@users.noreply.github.com>
2024-07-23 10:31:30 +0530

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,20 @@
+'''
+Comments are used to Comment the content i.e, which is the logic behind the code.
+There are two types of comments in python
+1.Singel line comment
+2.Multiple line comment
+
+'''
+
+
+
+
+
+# Singel line comment
+
+print("singel Line comment")
+
+
+# Multiple line comment
+
+print("Multiple line Coomets")
