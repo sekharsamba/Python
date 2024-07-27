82ba5136a7c3a6e523a33af1f98d8ae5dc62839e
Sekhar Pulakanti <114207278+sekharsamba@users.noreply.github.com>
2024-07-27 18:56:17 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,14 @@
+'''
+
+1.Sets are used to store multiple items in a single variable.
+2.Sets are written with curly brackets.
+3.Set items are unordered, unchangeable, and do not allow duplicate values.
+
+'''
+# set
+myset = {"apple", "banana", "cherry"}
+print(myset)    # {'banana', 'apple', 'cherry'}
+
+# duplicates not allowed
+fruits = {"apple", "banana", "cherry", "apple"}
+print(fruits)   # {'banana', 'cherry', 'apple'}
