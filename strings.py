a95af91342d883a81144e003647fde1101d81696
Sekhar Pulakanti <114207278+sekharsamba@users.noreply.github.com>
2024-07-27 18:52:43 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,18 @@
+# String 
+# Enclosed with " " , ' ', """ """ ,''' '''
+
+
+name = "Samba"
+print(name, type(name))   # Samba <class 'str'>
+
+car = 'volvo'
+print(car)     # volvo
+
+fruit = ''' Kiwi'''
+print(fruit)    #kiwi
+
+phone = """ Samsung"""
+print(phone)    # Samsung
+
+
+
