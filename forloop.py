474ee950244c286a16cb4ea889d2dc8e642bc4f5
Sekhar Pulakanti <114207278+sekharsamba@users.noreply.github.com>
2024-07-27 18:59:10 +0530

    Update forloop.py

--- forloop.py
+++ forloop.py
@@ -14,7 +14,7 @@ for i in range(0,10):
      print(i)        # 0 1 2 3 4 5 6 7 8 9
 
 
- # for loop range
+# for loop range
 for x in range(0,20,2):
     print(x)        # 0 2 4 6 8 10 12 14 16 18 
 
